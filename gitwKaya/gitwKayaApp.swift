e073e582fa6a2053e82873e94669af115e1cfbb7
Yunji Lee <yundlee15@gmail.com>
2024-04-18 20:00:52 +0900

    Initial Commit

---
+++ gitwKaya/gitwKayaApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitwKayaApp.swift
+//  gitwKaya
+//
+//  Created by 이윤지 on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct gitwKayaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
