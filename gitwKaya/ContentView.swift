21f72c24f19a8a6a75067f5a52da33bf40abc790
Yunji Lee <yundlee15@gmail.com>
2024-04-18 20:29:32 +0900

    First commit

--- gitwKaya/ContentView.swift
+++ gitwKaya/ContentView.swift
@@ -7,13 +7,66 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.yellow)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.green)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.blue)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.purple)
+            }
+            HStack{
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.yellow)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.green)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.blue)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.purple)
+            }
+            HStack{
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.red)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.yellow)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.green)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.blue)
+                Image(systemName: "cat")
+                    .imageScale(.large)
+                    .foregroundStyle(.purple)
+            }
+            .padding(.bottom,10)
+            Text("YA!ONG!s!")
+                .font(.title)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
         }
         .padding()
     }
